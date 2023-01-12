40e58851bcc5b47738e15908dc95feec9ebe5309
Jaehyeon <jaehyeonkim2358@gmail.com>
2023-01-13 04:02:53 +0900

    [FEAT] signaling event handler 추가

---
+++ src/signaling/signaling.gateway.ts
@@ -0,0 +1,54 @@
+import {
+    SubscribeMessage,
+    WebSocketGateway,
+    ConnectedSocket,
+    MessageBody,
+    WebSocketServer,
+} from '@nestjs/websockets';
+import { ConfigService } from '@nestjs/config';
+import { Server, Socket } from 'socket.io';
+
+const config = new ConfigService();
+const ORIGIN = config.get<string>('SOCKET_ORIGIN');
+const CREDENTIALS = config.get<boolean>('SOCKET_SIGNALING_CREDENTIALS');
+
+@WebSocketGateway({
+    cors: {
+        origin: ORIGIN,
+        credentials: CREDENTIALS,
+    },
+})
+export class SignalingGateway {
+    @WebSocketServer()
+    server: Server;
+
+    @SubscribeMessage('connection')
+    handleConnection(@ConnectedSocket() client: Socket) {}
+
+    @SubscribeMessage('join_room')
+    handleJoinRoom(
+        @ConnectedSocket() client: Socket,
+        @MessageBody() data: any,
+    ) {
+        let [roomId, newSocketId] = data;
+        client.to(roomId).emit('welcome', newSocketId);
+    }
+
+    @SubscribeMessage('offer')
+    handleOffer(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
+        let [offer, newSocketId, oldSocketId] = data;
+        client.to(newSocketId).emit('offer', offer, oldSocketId);
+    }
+
+    @SubscribeMessage('answer')
+    handleAnswer(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
+        let [answer, oldSocketId, newSocketId] = data;
+        client.to(oldSocketId).emit('answer', answer, newSocketId);
+    }
+
+    @SubscribeMessage('ice')
+    handleWelcome(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
+        let [ice, peerSocketId, currentSocketId] = data;
+        client.to(peerSocketId).emit(ice, currentSocketId);
+    }
+}
