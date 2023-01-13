65f604328af7f4ef5ccb760ac079b4fa53f7ec39
Jaehyeon <jaehyeonkim2358@gmail.com>
2023-01-13 18:07:21 +0900

    [FIX] origin 수정

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -1,5 +1,5 @@
 export const Config = {
-    SOCKET_ORIGIN: 'http://143.248.229.89:3000',
+    SOCKET_ORIGIN: 'http://192.168.0.84:80',
     SOCKET_SIGNALING_CREDENTIALS: true,
 };
 export default Config;
