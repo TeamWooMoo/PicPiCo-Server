3b3e88fbd8595d8004c718e21f0817812eb38ad0
Jaehyeon <jaehyeonkim2358@gmail.com>
2023-01-14 02:57:13 +0900

    [FEAT] mongoDB 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { SocketModule } from './socket/socket.module';
 // import { UploadModule } from './uploads/uploads.module';
+import { DrawingGateway } from './socket/drawing/drawing.gateway';
+import { UsersModule } from './users/users.module';
 
 @Module({
-    imports: [SocketModule],
+    imports: [SocketModule, UsersModule],
     controllers: [AppController],
+    providers: [DrawingGateway],
 })
 export class AppModule {}
