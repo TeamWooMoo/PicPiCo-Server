7d27d6b73a1631a326fe3c6b3a82047c96e3e84a
Jaehyeon <jaehyeonkim2358@gmail.com>
2023-01-14 14:34:08 +0900

    [FEAT] mongoDB 연결

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Req, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UserDto } from './users.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Post()
+    async signUp(@Body() newUser: UserDto) {
+        await this.usersService.create(newUser);
+        console.log(newUser);
+        return { newUser };
+    }
+
+    @Get()
+    async login(@Query() query: any) {
+        let { userId, name } = query;
+        console.log(`${userId}, ${name}`);
+        const result = await this.usersService.fineOne(userId);
+        console.log(result);
+    }
+}
