7ccd0091e8642cc4e7c8f68c5bc2ea3fa1d5140b
Jaehyeon <jaehyeonkim2358@gmail.com>
2023-01-15 15:10:54 +0900

    [ETC] config: origin 수정

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,12 @@
 const CORS = {
     // ORIGIN: 'http://192.168.0.84:3000', // JNL
     // ORIGIN: 'http://192.168.0.49:3000', // KJH
-    ORIGIN: ['http://192.168.0.84:3000', 'http://192.168.0.49:3000'],
+    // ORIGIN: 'http://192.168.0.15:3000', // CDB
+    ORIGIN: [
+        'http://192.168.0.84:3000',
+        'http://192.168.0.49:3000',
+        'http://192.168.0.15:3000',
+    ],
     // ORIGIN: 'https://picpico.site', // [진짜 서버] 배포용
     // ORIGIN: '*', // [진짜 서버] 개발용
     CREDENTIALS: true,
@@ -9,7 +14,11 @@ const CORS = {
 
 const Socket = {
     // SOCKET_ORIGIN: 'http://192.168.0.84:3000', // JNL
-    SOCKET_ORIGIN: ['http://192.168.0.84:3000', 'http://192.168.0.49:3000'],
+    SOCKET_ORIGIN: [
+        'http://192.168.0.84:3000',
+        'http://192.168.0.49:3000',
+        'http://192.168.0.15:3000',
+    ],
     // SOCKET_ORIGIN: 'http://192.168.0.49:3000', // KJH
     // SOCKET_ORIGIN: 'https://picpico.site', // [진짜 서버] 배포용
     // SOCKET_ORIGIN: '*', // [진짜 서버] 개발용
