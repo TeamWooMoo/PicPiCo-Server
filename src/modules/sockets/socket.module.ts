38db5555ff3dbf8287c4bca68f5f26b031072507
Jaehyeon <jaehyeonkim2358@gmail.com>
2023-01-21 10:36:30 +0900

    [FEAT] 스티커 이벤트 gateway 분리 및 추가

--- src/modules/sockets/socket.module.ts
+++ src/modules/sockets/socket.module.ts
@@ -5,6 +5,7 @@ import { DrawingGateway } from './gateways/drawing.gateway';
 import { RoomsModule } from '../rooms/rooms.module';
 import { SelectionGateway } from './gateways/selection.gateway';
 import { DecoGateway } from './gateways/deco.gateway';
+import { StickerGateway } from './gateways/sticker.gateway';
 
 @Module({
     imports: [RoomsModule],
@@ -12,6 +13,7 @@ import { DecoGateway } from './gateways/deco.gateway';
         SignalingGateway,
         CameraGateway,
         DrawingGateway,
+        StickerGateway,
         SelectionGateway,
         DecoGateway,
     ],
