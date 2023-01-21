130b2fac30f94ff0f91bc031497af70b3fa5f72a
Jaehyeon <jaehyeonkim2358@gmail.com>
2023-01-21 13:28:00 +0900

    [FIX] disconnect 이벤트 리스너 11개 이상 등록현상 수정_09

--- src/modules/sockets/socket.module.ts
+++ src/modules/sockets/socket.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { RoomsModule } from '../rooms/rooms.module';
 import { SignalingGateway } from './gateways/signaling.gateway';
 import { CameraGateway } from './gateways/camera.gateway';
 import { DrawingGateway } from './gateways/drawing.gateway';
-import { RoomsModule } from '../rooms/rooms.module';
 import { SelectionGateway } from './gateways/selection.gateway';
 import { DecoGateway } from './gateways/deco.gateway';
 import { StickerGateway } from './gateways/sticker.gateway';
