bdd272064cb3e5b5ed4226374b5f7e07570fe970
Jaehyeon <jaehyeonkim2358@gmail.com>
2023-01-23 02:27:56 +0900

    [ETC] sticker_move 테스트

--- src/modules/sockets/gateways/sticker.gateway.ts
+++ src/modules/sockets/gateways/sticker.gateway.ts
@@ -1,10 +1,4 @@
-import {
-    SubscribeMessage,
-    WebSocketGateway,
-    ConnectedSocket,
-    MessageBody,
-    WebSocketServer,
-} from '@nestjs/websockets';
+import { SubscribeMessage, WebSocketGateway, ConnectedSocket, MessageBody, WebSocketServer } from '@nestjs/websockets';
 import { MyServer, MySocket } from '../socket.interface';
 import { Config } from '../../../config/configuration';
 // import { RoomsService } from 'src/modules/rooms/rooms.service';
@@ -22,10 +16,7 @@ export class StickerGateway {
     server: MyServer;
 
     @SubscribeMessage('pick_sticker')
-    async handlePickSticker(
-        @ConnectedSocket() client: MySocket,
-        @MessageBody() data: any,
-    ) {
+    async handlePickSticker(@ConnectedSocket() client: MySocket, @MessageBody() data: any) {
         let [url, targetImgIdx] = data;
         console.log('pick_sticker : ', data);
 
@@ -34,10 +25,7 @@ export class StickerGateway {
     }
 
     @SubscribeMessage('sticker_on')
-    async handleStickerOn(
-        @ConnectedSocket() client: MySocket,
-        @MessageBody() data: any,
-    ) {
+    async handleStickerOn(@ConnectedSocket() client: MySocket, @MessageBody() data: any) {
         const [roomId, stickerId] = data;
 
         client.emit('sticker_on');
@@ -45,11 +33,9 @@ export class StickerGateway {
     }
 
     @SubscribeMessage('sticker_move')
-    async handleStickerMove(
-        @ConnectedSocket() client: MySocket,
-        @MessageBody() data: any,
-    ) {
+    async handleStickerMove(@ConnectedSocket() client: MySocket, @MessageBody() data: any) {
         const [left, top, imgIdx, src] = data;
+        console.log(data);
 
         client.to(client.myRoomId).emit('sticker_move', left, top, imgIdx, src);
     }
