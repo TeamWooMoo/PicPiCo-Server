357ee3490fd69bc591b9e83fc5629b89b26307de
Jaehyeon <112854083+jaehyeonkim2358@users.noreply.github.com>
2023-01-23 15:35:17 +0900

    [FIX] nodejs fs.rmdir -> fd.rm 으로 수정3

--- src/cache/redis.service.ts
+++ src/cache/redis.service.ts
@@ -8,7 +8,7 @@ export class RedisService {
     constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {
         this.reset();
         const fs = require('fs');
-        fs.rm('./static', { recursive: true }, (err) => {
+        fs.rmSync('./static', { recursive: true }, (err) => {
             if (err) {
                 console.log(err);
             } else {
